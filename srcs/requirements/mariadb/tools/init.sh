969495b4ba467836ea37d4c4257190bf4888d456
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-02-17 23:25:02 -0300

    [Update] trying to fix database connection

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -2,6 +2,22 @@
 
 service mariadb start
 
+# Install MariaDB
+mysql_secure_installation << _EOF_
+
+Y
+1234root4321
+1234root4321
+Y
+n
+Y
+Y
+_EOF_
+
+# Grant root access to MariaDB
+mariadb -e "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mariadb -e "FLUSH PRIVILEGES;"
+
 # Create WordPress database
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
