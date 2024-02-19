bfe62e90d6a6d706d1a2e6e9aa74e7144c7f5cd1
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-02-19 00:35:29 -0300

    [Update] trying to connect redis

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -6,24 +6,27 @@
 wget https://wordpress.org/latest.tar.gz
 tar -xvf latest.tar.gz
 mkdir -p /var/www/html/
-mv wordpress /var/www/html/
+mv wordpress/* /var/www/html/
 rm -rf latest.tar.gz
 
 # Configure WordPress config with database information
-cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
-sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wordpress/wp-config.php
-sed -i "s/localhost/$MYSQL_HOSTNAME:3306/g" /var/www/html/wordpress/wp-config.php
+cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+sed -i "s/database_name_here/$MYSQL_DATABASE/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wp-config.php
+sed -i "s/localhost/$MYSQL_HOSTNAME:3306/g" /var/www/html/wp-config.php
 
 # Change ownership of WordPress files
-chown -R www-data:www-data /var/www/html/wordpress
+chown -R www-data:www-data /var/www/html/
 
 ##################  BONUS  ##################
 
 # Redis Cache
-echo "\n################## Redis Cache ##################\n" >> /var/www/html/wordpress/wp-config.php
-echo "define('WP_REDIS_HOST', 'redis');" >> /var/www/html/wordpress/wp-config.php
-echo "define('WP_REDIS_PORT', '6379');" >> /var/www/html/wordpress/wp-config.php
-echo "define('WP_REDIS_PASSWORD', '$REDIS_PASSWORD');" >> /var/www/html/wordpress/wp-config.php
-echo "define('WP_CACHE', true);" >> /var/www/html/wordpress/wp-config.php
+
+echo -e "\n\
+################## Redis Cache ##################\n\
+define('WP_CACHE', true);\n\
+define('WP_REDIS_HOST', 'redis');\n\
+define('WP_REDIS_PORT', '6379');\n\
+define('WP_REDIS_TIMEOUT', 1);\
+" | tee -a /var/www/html/wp-config.php
