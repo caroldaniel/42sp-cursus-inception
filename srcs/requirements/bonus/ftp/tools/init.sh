6f25d1d62c61fda5a72271651fec0050a83785e8
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-02-22 16:08:59 -0300

    [Update] FTP server working

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
+if [ -z "$FTP_USER" ] || [ -z "$FTP_PASSWORD" ]; then
+    echo "FTP_USER and FTP_PASSWORD must be set"
+    exit 1
+fi
+
+if [ ! -f /usr/local/bin/vsftpd.conf.bak ]; then
+
+    # Create empty directory for the FTP server
+    mkdir -p /var/run/vsftpd/empty
+
+    # Create a user and a home directory for it
+    adduser --disabled-password $FTP_USER --gecos ""
+    mkdir -p /home/$FTP_USER/ftp
+
+    # Set the user's password
+    echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
+
+    # Set the user's home directory
+    chown -R $FTP_USER:$FTP_USER /home/$FTP_USER
+
+    # Set permissions for the user's home directory
+    chmod 755 /home/$FTP_USER/ftp
+
+    # Add the user to the FTP server
+
+    echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
+
+    # Copy the configuration file to the FTP server
+    cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+    mv /usr/local/bin/vsftpd.conf /etc/vsftpd.conf
+    sed -i "s/^local_root=.*/local_root=\/home\/$FTP_USER\/ftp/" /etc/vsftpd.conf
+    chmod 644 /etc/vsftpd.conf
+
+    # Unset variables
+    unset FTP_USER
+    unset FTP_PASSWORD
+
+fi
+
 # Start the FTP server
-service vsftpd start
-
-# Add the FTP user, change the password and declare him the owner of the FTP directory
-adduser $FTP_USER --disabled-password
-echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-echo "$FTP_USER" | tee -a /etc/vsftpd/vsftpd.userlist
-
-# Create the repository for the FTP server
-mkdir -p /home/ftp
-chown -R nobody:nogroup /home/ftp
-chmod a-w /home/ftp
-
-# Create the repository for the FTP files
-mkdir -p /home/ftp/wordpress
-chown -R $FTP_USER:$FTP_USER /home/ftp/wordpress
-
-# Configure the FTP server via vsftpd.conf file
-sed -i 's/#write_enable=YES/write_enable=YES/' /etc/vsftpd/vsftpd.conf
-sed -i 's/#chroot_local_user=YES/chroot_local_user=YES/' /etc/vsftpd/vsftpd.conf
-
-echo -e "\n\
-local_enable=YES\n\
-allow_writeable_chroot=YES\n\
-local_root=/home/ftp/\n\
-pasv_enable=YES\n\
-pasv_min_port=21000\n\
-pasv_max_port=21010\n\
-userlist_file=/etc/vsftpd/vsftpd.userlist\
-" | tee -a /etc/vsftpd/vsftpd.conf
-
-service vsftpd stop
+vsftpd /etc/vsftpd.conf
