76841464789a99eb0cce4a333c083f8c9adc6a6d
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-02-27 20:14:20 -0300

    [Uṕdate] cAdvisor

--- srcs/requirements/bonus/adminer/tools/entrypoint.sh
+++ srcs/requirements/bonus/adminer/tools/entrypoint.sh
@@ -3,5 +3,5 @@
 # Set up Adminer
 mkdir -p /var/www/html
 wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
-chown -R www-data:www-data /var/www/html
+chown www-data:www-data /var/www/html/adminer.php
 chmod 755 /var/www/html/adminer.php
