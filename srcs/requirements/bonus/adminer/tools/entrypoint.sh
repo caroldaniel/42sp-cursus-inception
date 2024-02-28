c0da02f92a820b18b5dc660155ae35d8984539cb
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-02-27 21:54:50 -0300

    [Update] Adminer Entrypoint

--- srcs/requirements/bonus/adminer/tools/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set up Adminer
-mkdir -p /var/www/html
-wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer.php
-chown www-data:www-data /var/www/html/adminer.php
-chmod 755 /var/www/html/adminer.php
